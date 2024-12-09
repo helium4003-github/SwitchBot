f02f9b3c5675f94e17ef597d515baf31dd9dd151
helium4003-github <helium4003main@gmail.com>
2024-12-09 20:01:31 +0900

    ボタンが押されたら起動するだけのやつ

--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,27 @@ int bule = 0x0000f0;
 int red = 0xff0000;
 int yellow = 0xfff000;  
 //調整用引数
-int sleep = 500;
+uint32_t dly = 500;
 int defult = 160;
 int push = 180;
 int pull = 140;
 
 void Push(){
   servo.write(push);   
-  delay(sleep);      
+  delay(dly);      
   servo.write(defult);          
-  delay(sleep);    
+  delay(dly);    
 }
 
 void Pull(){
   servo.write(pull);   
-  delay(sleep);      
+  delay(dly);      
   servo.write(defult);          
-  delay(sleep);    
+  delay(dly);    
 }
 
 void setup() {
-  M5.begin(true, false,true); 
-  delay(50);
+  M5.begin(true, false, true);
   M5.dis.drawpix(0, 0x00ff00);
   servo.attach(25);
   servo.write(0);
@@ -40,7 +39,8 @@ void setup() {
 void loop() {
   M5.update(); 
   M5.dis.drawpix(0, green);
-  while (!M5.Btn.wasPressed()) {}
-  Push();
-  M5.dis.drawpix(0, red);
+  if (!M5.Btn.wasPressed()) {
+    M5.dis.drawpix(0, red);
+    Push();
+  }  
 }
