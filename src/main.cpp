42de196ce15e3ad6372cd401daec616c9828b592
helium4003-github <helium4003main@gmail.com>
2024-12-23 08:44:27 +0900

    プログラムの簡略化

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 #include "M5Atom.h"
 #include "ESP32Servo.h"
 
-Servo servo;
+Servo button;
 //色指定用引数
 int green = 0x00ff00;
 int bule = 0x0000f0;
 int red = 0xff0000;
-int yellow = 0xfff000;  
+int yellow = 0xfff000;
 //調整用引数
 uint32_t dly = 500;
 int defult = 160;
@@ -15,32 +15,29 @@ int push = 180;
 int pull = 140;
 
 void Push(){
-  servo.write(push);   
-  delay(dly);      
-  servo.write(defult);          
-  delay(dly);    
+  M5.dis.drawpix(0, red);
+  button.write(push);
+  delay(dly);
+  button.write(defult);
 }
 
 void Pull(){
-  servo.write(pull);   
-  delay(dly);      
-  servo.write(defult);          
-  delay(dly);    
+  M5.dis.drawpix(0, red);
+  button.write(pull);
+  delay(dly);
+  button.write(defult);
 }
 
 void setup() {
   M5.begin(true, false, true);
-  M5.dis.drawpix(0, 0x00ff00);
-  servo.attach(25);
-  servo.write(0);
-  servo.write(defult);
+  M5.dis.drawpix(0, green);
+  button.attach(25);
+  button.write(defult);
 }
 
 void loop() {
   M5.update(); 
   M5.dis.drawpix(0, green);
-  if (!M5.Btn.wasPressed()) {
-    M5.dis.drawpix(0, red);
-    Push();
-  }  
+  while(!M5.Btn.wasPressed()) {}
+  Push();  
 }
